857414a846eb6f8c0a18bb2a7fbb5a89094ecf6f
APeacock <apeacock@apeacock-m1.roundarch.com>
2014-12-23 11:35:42 -0600

    removed add script for DB

--- routes/index.js
+++ routes/index.js
@@ -859,89 +859,6 @@ var brewSchema= [
     ]
   }
 ];
-var brewSchema2= [
-  {
-    name: "Revolution Brewing",
-    utvid: "2426",
-    address: "2323 North Milwaukee Avenue, Chicago, IL 60647",
-    phone: "(773) 227-2739",
-    menu: "http://revbrew.com/brewpub/pub-menu",
-    lat: "41.923618",
-    lng: "-87.69814"
-  },
-  {
-    name: "Half Acre Beer Company",
-    utvid: "8453",
-    address: "4257 North Lincoln Avenue, Chicago, IL 60618",
-    phone: "(773) 248-4038",
-    menu: "http://www.viewmenu.com/half-acre-tap-room/menu",
-    lat: "41.9593418",
-    lng: "-87.6819468"
-  },
-  {
-    name: "Haymarket Pub & Brewery",
-    utvid: "6962",
-    address: "737 West Randolph Street, Chicago, IL 60661",
-    phone: "(312) 638-0700",
-    menu: "https://locu.com/places/haymarket-pub-brewery-chicago-us/#menu",
-    lat: "41.884177",
-    lng: "-87.647249"
-  },
-  {
-    name: "Atlas Brewing Company",
-    utvid: "298942",
-    address: "2747 North Lincoln Avenue, Chicago, IL 60614",
-    phone: "(773) 295-1270",
-    menu: "http://www.urbanspoon.com/cities/2-chicago/restaurants/1714435-atlas-brewing-company/menu",
-    lat: "41.931876",
-    lng: "-87.657292"
-  },
-  {
-    name: "DryHop Brewers",
-    utvid: "225374",
-    address: "3155 North Broadway Street, Chicago, IL 60657", //correct address is 3155, but results are fucked sometimes if 3155 used
-    phone: "(773) 857-3155",
-    menu: "http://www.urbanspoon.com/cities/2-chicago/restaurants/1757835-dryhop-brewers/menu",
-    lat: "41.939351",
-    lng: "-87.644205"
-  },
-  {
-    name: "Goose Island",
-    utvid: "881",
-    address: "1800 North Clybourn Avenue, Chicago, IL 60614",
-    phone: "(312) 915-0071",
-    menu: "http://www.gayot.com/restaurants/menu/goose-island-brewery-chicago-il-60614_5ch99343.html",
-    lat: "41.913696",
-    lng: "-87.653783"
-  },
-  {
-    name: "Begyle Brewing Company",
-    utvid: "386651",
-    address: "1800 West Cuyler Avenue, Chicago, IL 60613", //ZIP code is weird, one says 60625 one says 60613s
-    phone: "(773) 661-6963",
-    menu: null,
-    lat: "41.955284",
-    lng: "-87.674506"
-  },
-  {
-    name: "Piece Brewery & Pizzeria",
-    utvid: "3852",
-    address: "1927 West North Avenue, Chicago, IL 60622",
-    phone: "(773) 772-4422",
-    menu: "http://www.viewmenu.com/piece/menu",
-    lat: "41.910327",
-    lng: "-87.676104"
-  },
-  {
-    name: "Lagunitas Brewing Company Chicago",
-    utvid: "1773303",
-    address: "1843 South Washtenaw Avenue, Chicago, IL 60608",
-    phone: "(707) 769-4495",
-    menu: null,
-    lat: "41.857202",
-    lng: "-87.691988"
-  }
-];
 //SCHEMA NOTES
 /*
 ~ beer list
@@ -952,12 +869,12 @@ x location + address
 - description
 x contact info
 */
-for(var z= 0; z< brewSchema.length; z++) {
-  var brewery = new db.Brewery(brewSchema[z]);
-  db.dbsave(brewery, function() {
-    console.log("saved");
-  });
-}
+// for(var z= 0; z< brewSchema.length; z++) {
+//   var brewery = new db.Brewery(brewSchema[z]);
+//   db.dbsave(brewery, function() {
+//     console.log("saved");
+//   });
+// }
 /* GET home page. */
 router.get('/', function(req, res) {
   db.tester(function(breweries) {
